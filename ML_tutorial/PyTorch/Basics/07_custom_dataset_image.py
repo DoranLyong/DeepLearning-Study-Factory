fb08db98ad67f09732504b8d4f5e277dae4c322b
DoranLyong <cheeryun@gmail.com>
2021-01-16 22:06:22 +0900

    simple_saveload

---
+++ ML_tutorial/PyTorch/Basics/07_custom_dataset_image.py
@@ -0,0 +1,9 @@
+# coding=<utf-8> 
+"""
+(ref) https://youtu.be/ZoZHd0Zm3RY
+(ref) https://github.com/aladdinpersson/Machine-Learning-Collection/blob/aba36b89b438ca8f608a186f4d61d1b60c7f24e0/ML/Pytorch/Basics/custom_dataset/custom_dataset.py#L12-L29
+
+* Run this code on VScode (it can be run with Jupyter notebook conntection)
+* Run each code cell with pressing 'shift + Enter'
+
+"""
